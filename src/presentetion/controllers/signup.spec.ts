65d21033acbf1f608db64d2d2a36248ce1cbd7ac
Nilson Souza <souza_arj@hotmail.com>
2020-08-26 16:07:08 -0300

    test: ensure SignupController returns 400 if password confirmation fails

--- src/presentetion/controllers/signup.spec.ts
+++ src/presentetion/controllers/signup.spec.ts
@@ -90,6 +90,22 @@ describe('Signup Controller', () => {
     expect(httpResponse.body).toEqual(new MissingParamError('passwordConfirmation'))
   })
 
+  test('Should return 400 if passwordConfirmation fails', () => {
+    // sut = system under test
+    const { sut } = makeSut()
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_email@MediaList.com',
+        password: 'any_password',
+        passwordConfirmation: 'invalid_password'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new InvalidParamError('passwordConfirmation'))
+  })
+
   test('Should return 400 if an invalid email is provided', () => {
     // sut = system under test
     const { sut, emailValidatorStub } = makeSut()
