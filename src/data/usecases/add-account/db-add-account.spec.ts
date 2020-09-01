b98a7b371378a28e86c46ed8cfd831dad89abe40
Nilson Souza <souza_arj@hotmail.com>
2020-09-01 16:38:45 -0300

    test: ensure DbAccount returns an account on success

--- src/data/usecases/add-account/db-add-account.spec.ts
+++ src/data/usecases/add-account/db-add-account.spec.ts
@@ -1,6 +1,6 @@
 
-import { Encrypter, AddAccountModel, AccountModel, AddAccountRepository } from './add-account/db-add-account-protocols'
-import { DbAddAccount } from './add-account/db-add-account'
+import { Encrypter, AddAccountModel, AccountModel, AddAccountRepository } from './db-add-account-protocols'
+import { DbAddAccount } from './db-add-account'
 
 const makeEncrypter = (): Encrypter => {
   class EncrypterStub implements Encrypter {
@@ -87,6 +87,7 @@ describe('DbAddAccount Usecase', () => {
   test('Should throw if AddAccountRepository throws', async () => {
     const { sut, addAccountRepositoryStub } = makeStub()
     jest.spyOn(addAccountRepositoryStub, 'add').mockReturnValueOnce(Promise.reject(new Error()))
+
     const accountData = {
       name: 'valid_name',
       email: 'valid_email',
@@ -94,5 +95,21 @@ describe('DbAddAccount Usecase', () => {
     }
     const accountPromise = sut.add(accountData)
     await expect(accountPromise).rejects.toThrow()
+  })
+
+  test('Should return an account on success', async () => {
+    const { sut } = makeStub()
+    const accountData = {
+      name: 'valid_name',
+      email: 'valid_email',
+      password: 'valid_password'
+    }
+    const account = await sut.add(accountData)
+    expect(account).toEqual({
+      id: 'valid_id',
+      name: 'valid_name',
+      email: 'valid_email',
+      password: 'hashed_password'
+    })
   })
 })
