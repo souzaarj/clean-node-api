c2eb4c9116dfb7663c1c3c89f40dc8ca7bd1fd63
Nilson Souza <souza_arj@hotmail.com>
2020-09-04 11:59:16 -0300

    test: ensure AcccountMongoRepository return an account on success

---
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -0,0 +1,27 @@
+import { AddAccountMongoRepository } from './account'
+import { MongoHelper } from '../helpers/mongo-helper'
+
+describe('Account Mongo Repository', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL ?? 'mongodb://localhost:27017')
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  test('Should return an account on success', async () => {
+    const sut = new AddAccountMongoRepository()
+    const account = await sut.add({
+      name: 'any_name',
+      email: 'any_email',
+      password: 'any_password'
+    })
+
+    expect(account).toBeTruthy()
+    expect(account.id).toBeTruthy()
+    expect(account.name).toBe('any_name')
+    expect(account.email).toBe('any_email')
+    expect(account.password).toBe('any_password')
+  })
+})
