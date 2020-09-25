f3ad179a21ecf0b97cd883ffdfe00a51ba84171b
Nilson Souza <souza_arj@hotmail.com>
2020-09-25 12:47:23 -0300

    test: ensure SigninController returns 400 if no user is provide

---
+++ src/presentation/controllers/signin/signin.spec.ts
@@ -0,0 +1,19 @@
+import { HttpRequest } from './../../protocols/http'
+import { SinginController } from './signin'
+
+const makeSut = (): SinginController => {
+  return new SinginController()
+}
+
+describe('Signin Controller', () => {
+  test('Should returns 400 if no user is provide', async () => {
+    const sut = makeSut()
+    const httpRequest: HttpRequest = {
+      body: {
+        password: 'any_password'
+      }
+    }
+    const httpResponse = await sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+  })
+})
