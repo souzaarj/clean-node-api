7bd30e9be5ebcd76bd2a189d10f5e7843d8b17cd
Nilson Souza <souza_arj@hotmail.com>
2020-09-25 13:02:56 -0300

    test: ensure SigninController returns 400 if no password is provide

--- src/presentation/controllers/signin/signin.spec.ts
+++ src/presentation/controllers/signin/signin.spec.ts
@@ -16,4 +16,15 @@ describe('Signin Controller', () => {
     const httpResponse = await sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
   })
+
+  test('Should returns 400 if no password is provide', async () => {
+    const sut = makeSut()
+    const httpRequest: HttpRequest = {
+      body: {
+        user: 'any_user'
+      }
+    }
+    const httpResponse = await sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+  })
 })
