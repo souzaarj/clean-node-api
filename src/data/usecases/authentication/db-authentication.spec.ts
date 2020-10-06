bdbc43cf4786cb41beedf51adcf114252c27dbea
Nilson Souza <souza_arj@hotmail.com>
2020-10-06 17:40:22 -0300

    test: ensure DBAuthentication returns null if Hashcompare returns false

--- src/data/usecases/authentication/db-authentication.spec.ts
+++ src/data/usecases/authentication/db-authentication.spec.ts
@@ -89,4 +89,11 @@ describe('DbAuthentication UseCase', () => {
     const promise = sut.auth(makeFakeAuthentication())
     await expect(promise).rejects.toThrow()
   })
+
+  test('Should return null if HashComparer returns false', async () => {
+    const { sut, hashComparerStub } = makeSut()
+    jest.spyOn(hashComparerStub, 'compare').mockResolvedValueOnce(false)
+    const accessToken = await sut.auth(makeFakeAuthentication())
+    await expect(accessToken).toBeNull
+  })
 })
