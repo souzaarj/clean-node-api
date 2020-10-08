afb06c6c71957794ca3bd91f9af5b6ba1a2842b5
Nilson Souza <souza_arj@hotmail.com>
2020-10-08 20:44:03 -0300

    feat: ensure BcryptAdaper returns true when compare success

--- src/infra/criptography/bcrypt-adapter.spec.ts
+++ src/infra/criptography/bcrypt-adapter.spec.ts
@@ -52,4 +52,10 @@ describe('Bcrypt Adapter', () => {
     await sut.compare('any_value', 'any_hash')
     expect(compareSpy).toBeCalledWith('any_value', 'any_hash')
   })
+
+  test('Should return true when compare success', async () => {
+    const { sut } = makeSut()
+    const hash = await sut.compare('any_value','any_hash')
+    expect(hash).toBeTruthy()
+  })
 })
