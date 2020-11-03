33e76d007e3ba650e1f3c44c191145d44f9122e5
Nilson Souza <souza_arj@hotmail.com>
2020-11-02 22:40:32 -0300

    test: ensure DBLoadAccountByToken throws if any dependency throws

--- src/data/usecases/load-account-by-token/db-load-account-by-token.spec.ts
+++ src/data/usecases/load-account-by-token/db-load-account-by-token.spec.ts
@@ -78,4 +78,18 @@ describe('DBLoadAccountByToken Usecase', () => {
     const account = await sut.load('any_token', 'any_role')
     expect(account).toEqual(makeFakeAccount())
   })
+
+  test('Should throw if Decrypter throws ', async () => {
+    const { sut, decrypterStub } = makeSut()
+    jest.spyOn(decrypterStub, 'decrypt').mockRejectedValueOnce(new Error())
+    const promise = sut.load('any_token', 'any_role')
+    await expect(promise).rejects.toThrowError()
+  })
+
+  test('Should throw if LoadAccountByTokenRepository throws ', async () => {
+    const { sut, loadAccountByTokenRepositoryStub } = makeSut()
+    jest.spyOn(loadAccountByTokenRepositoryStub, 'loadByToken').mockRejectedValueOnce(new Error())
+    const promise = sut.load('any_token', 'any_role')
+    await expect(promise).rejects.toThrowError()
+  })
 })
