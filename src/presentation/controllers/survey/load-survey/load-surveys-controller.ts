718dc3eca7fa4b8e3c621105b5f036d266dd05ea
Nilson Souza <souza_arj@hotmail.com>
2020-11-14 10:18:10 -0300

    feat: ensure return 500 if LoadSurveyController throws

--- src/presentation/controllers/survey/load-survey/load-surveys-controller.ts
+++ src/presentation/controllers/survey/load-survey/load-surveys-controller.ts
@@ -1,10 +1,15 @@
+import { serverError } from './../../../helpers/http/http-helper'
 import { LoadSurveys } from './../../../../domain/usecases/survey/load-survey-protocols'
 import { HttpRequest, HttpResponse } from '../../../protocols'
 import { Controller } from '../../../protocols/controller'
 export class LoadSurveysController implements Controller {
   constructor (private readonly loadSurveys: LoadSurveys) { }
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    await this.loadSurveys.load()
-    return null
+    try {
+      await this.loadSurveys.load()
+      return null
+    } catch (error) {
+      return serverError(error)
+    }
   }
 }
