929cfab5bdc0396072dbda4e29bf7ee843f1f9f2
Nilson Souza <souza_arj@hotmail.com>
2020-11-18 09:50:41 -0300

    test: ensure load survey route returns 204 if a valid token is provided

--- src/main/routes/survey-routes.test.ts
+++ src/main/routes/survey-routes.test.ts
@@ -79,4 +79,31 @@ describe('GET /Surveys', () => {
       .get('/api/surveys')
       .expect(403)
   })
+
+  test('Should return 200 on load surveys with valid accessToken', async () => {
+    const res = await accountCollection.insertOne(
+      {
+        name: 'Rodrigo',
+        email: 'rodrido@email.com',
+        password: '123',
+        role: 'admin'
+      }
+    )
+
+    const id = res.ops[0]._id
+    const accessToken = sign({ id }, env.jwtSecret)
+
+    await accountCollection.updateOne({
+      _id: id
+    }, {
+      $set: {
+        accessToken
+      }
+    })
+
+    await request(app)
+      .get('/api/surveys')
+      .set('x-access-token', accessToken)
+      .expect(204)
+  })
 })
