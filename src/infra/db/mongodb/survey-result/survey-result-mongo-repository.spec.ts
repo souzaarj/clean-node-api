579063ca3008daefa8c4961337a2b867137e531c
Nilson Souza <souza_arj@hotmail.com>
2020-11-25 08:21:37 -0300

    test: ensure SurveyResultMongoRepository updated surveyResult if its not new

--- src/infra/db/mongodb/survey-result/survey-result-mongo-repository.spec.ts
+++ src/infra/db/mongodb/survey-result/survey-result-mongo-repository.spec.ts
@@ -1,6 +1,5 @@
 import { AccountModel } from '@/domain/models/account-protocols'
 import { SurveyModel } from '@/domain/models/survey-protocols'
-import { AddSurveyModel } from '@/domain/usecases/survey/add-survey-protocols'
 import { SurveyResultMongoRepository } from './survey-result-mongo-repository'
 import { MongoHelper } from '../helpers/mongo-helper'
 import { Collection } from 'mongodb'
@@ -8,20 +7,6 @@ import { Collection } from 'mongodb'
 let surveyResultCollection: Collection
 let accountCollection: Collection
 let surveyCollection: Collection
-
-const makeFakeAddSurvey = (): AddSurveyModel => (
-  {
-    question: 'any_question',
-    answers: [{
-      image: 'any_image',
-      answer: 'any_answer'
-    },
-    {
-      answer: 'other_answer'
-    }],
-    date: new Date()
-  }
-)
 
 const makeFakeAddAccount = async (): Promise<AccountModel> => {
   const result = await accountCollection.insertOne({
@@ -34,7 +19,17 @@ const makeFakeAddAccount = async (): Promise<AccountModel> => {
 }
 
 const makeSurvey = async (): Promise<SurveyModel> => {
-  const result = await surveyCollection.insertOne(makeFakeAddSurvey())
+  const result = await surveyCollection.insertOne({
+    question: 'any_question',
+    answers: [{
+      image: 'any_image',
+      answer: 'any_answer'
+    },
+    {
+      answer: 'other_answer'
+    }],
+    date: new Date()
+  })
   const survey = await MongoHelper.map(result.ops[0])
   return survey
 }
@@ -72,5 +67,27 @@ describe('SurveyResultMongo Repository', () => {
     expect(surveyResult).toBeTruthy()
     expect(surveyResult.id).toBeTruthy()
     expect(surveyResult.answer).toBe(survey.answers[0].answer)
+  })
+
+  test('Should update a result if its not new', async () => {
+    const sut = makeSut()
+    const survey = await makeSurvey()
+    const account = await makeFakeAddAccount()
+    const result = await surveyResultCollection.insertOne({
+      surveyId: survey.id,
+      accountId: account.id,
+      answer: survey.answers[0].answer,
+      date: new Date()
+    })
+
+    const surveyResult = await sut.save({
+      surveyId: survey.id,
+      accountId: account.id,
+      answer: survey.answers[1].answer,
+      date: new Date()
+    })
+    expect(surveyResult).toBeTruthy()
+    expect(surveyResult.id).toEqual(result.ops[0]._id)
+    expect(surveyResult.answer).toBe(survey.answers[1].answer)
   })
 })
