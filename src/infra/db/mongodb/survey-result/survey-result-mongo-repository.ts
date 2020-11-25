3be3c9fb0ef00bef2416a273c86e3c17f40e7004
Nilson Souza <souza_arj@hotmail.com>
2020-11-25 08:09:28 -0300

    feat: ensure SurveyResultMongoRepository insert new SurveyResult its new

---
+++ src/infra/db/mongodb/survey-result/survey-result-mongo-repository.ts
@@ -0,0 +1,26 @@
+import { SaveSurveyResultModel } from './../../../../domain/usecases/survey/save-survey-result-protocols'
+import { SaveSurveyResultRepository } from '@/data/protocols/db/survey/save-survey-result-repository'
+import { surveyResultModel } from '@/domain/models/survey-result-protocols'
+import { MongoHelper } from '../helpers/mongo-helper'
+
+export class SurveyResultMongoRepository implements SaveSurveyResultRepository {
+  async save (data: SaveSurveyResultModel): Promise<surveyResultModel> {
+    const surveyResultCollection = await MongoHelper.getCollection('surveyResults')
+    const surveyResult = await surveyResultCollection.findOneAndUpdate(
+      {
+        surveyId: data.surveyId,
+        accountId: data.accountId
+      }, {
+        $set: {
+          answer: data.answer,
+          date: data.date
+        }
+      },
+      {
+        upsert: true,
+        returnOriginal: false
+      })
+
+    return MongoHelper.map(surveyResult.value)
+  }
+}
