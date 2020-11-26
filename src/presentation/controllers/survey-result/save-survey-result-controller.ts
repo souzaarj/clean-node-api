83c90eb33c28598d94cbe22b46fafe18f14b7139
Nilson Souza <souza_arj@hotmail.com>
2020-11-26 14:27:54 -0300

    feat: ensure SaveSurveyResultController returns 400 if validate fails

--- src/presentation/controllers/survey-result/save-survey-result-controller.ts
+++ src/presentation/controllers/survey-result/save-survey-result-controller.ts
@@ -1,10 +1,14 @@
+import { badRequest } from '@/presentation/helpers/http/http-helper'
 import { Validation } from '@/presentation/protocols/validation'
 import { Controller } from '@/presentation/protocols/controller'
 import { HttpRequest, HttpResponse } from '../login/login/login-controller-protocols'
 export class SaveSurveyResultController implements Controller {
   constructor (private readonly validation: Validation) { }
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    this.validation.validate(httpRequest.body)
+    const validationError = this.validation.validate(httpRequest.body)
+    if (validationError) {
+      return badRequest(validationError)
+    }
     return null
   }
 }
