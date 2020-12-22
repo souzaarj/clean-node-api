05468bee715b26cf177e7daff2838b28274032ce
Nilson Souza <souza_arj@hotmail.com>
2020-12-22 19:02:51 -0300

    docs: add login api docs

--- src/main/docs/index.ts
+++ src/main/docs/index.ts
@@ -1,3 +1,7 @@
+import { loginParamsSchema } from './schemas/login-params-schema'
+import { accountSchema } from './schemas/account-schema'
+import { loginPath } from './login-path'
+
 export default {
   openapi: '3.0.0',
   info:
@@ -5,5 +9,18 @@ export default {
     title: 'Clean Node API',
     description: 'API do curso do Mango para realizar enquetes entre programadores.',
     version: '1.0.0'
+  },
+  servers: [{
+    url: '/api'
+  }],
+  tags: [{
+    name: 'Login'
+  }],
+  paths: {
+    '/login': loginPath
+  },
+  schemas: {
+    account: accountSchema,
+    loginParams: loginParamsSchema
   }
 }
