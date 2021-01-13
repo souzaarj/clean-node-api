7c185d539d550158dfcfdd0b7bca5ed65d00c166
Nilson Souza <souza_arj@hotmail.com>
2021-01-13 19:10:13 -0300

    test: ensure LoadSurveyResultController returns 200 on success

--- src/presentation/controllers/survey-result/load-survey-result/load-survey-result-controller.spec.ts
+++ src/presentation/controllers/survey-result/load-survey-result/load-survey-result-controller.spec.ts
@@ -2,37 +2,41 @@ import { mockSurveyResultModel } from '@/domain/test/mock-survey-result'
 import { LoadSurveyResult } from '@/domain/usecases/survey-result/load-survey-result-protocols'
 import { mockLoadSurveyById } from '@/presentation/test/mock-survey'
 import { ServerError, InvalidParamError } from '@/presentation/errors'
-import { forbidden, serverError } from '@/presentation/helpers/http/http-helper'
+import { forbidden, serverError, ok } from '@/presentation/helpers/http/http-helper'
 import { LoadSurveyById } from '@/domain/usecases/survey/load-survey-by-id-protocols'
 import { LoadSurveyResultController } from './load-survey-result-controller'
 import { SurveyResultModel } from '../save-survey-result/save-survey-result-protocols'
-
-const mockLoadSurveyResult = (): LoadSurveyResult => {
-  class LoadSurveyResultStub implements LoadSurveyResult {
-    async load (surveyId: string): Promise<SurveyResultModel> {
-      return mockSurveyResultModel()
-    }
-  }
-  return new LoadSurveyResultStub()
-}
-
-type SutTypes = {
-  sut: LoadSurveyResultController
-  loadSurveyByIdStub: LoadSurveyById
-  loadSurveyResultStub: LoadSurveyResult
-}
-const makeSut = (): SutTypes => {
-  const loadSurveyByIdStub = mockLoadSurveyById()
-  const loadSurveyResultStub = mockLoadSurveyResult()
-  const sut = new LoadSurveyResultController(loadSurveyByIdStub, loadSurveyResultStub)
-  return {
-    sut,
-    loadSurveyByIdStub,
-    loadSurveyResultStub
-  }
-}
+import mockdate from 'mockdate'
 
 describe('LoadSurveyResult Controller', () => {
+  beforeAll(() => mockdate.set(new Date()))
+  afterAll(() => mockdate.reset())
+
+  const mockLoadSurveyResult = (): LoadSurveyResult => {
+    class LoadSurveyResultStub implements LoadSurveyResult {
+      async load (surveyId: string): Promise<SurveyResultModel> {
+        return mockSurveyResultModel()
+      }
+    }
+    return new LoadSurveyResultStub()
+  }
+
+  type SutTypes = {
+    sut: LoadSurveyResultController
+    loadSurveyByIdStub: LoadSurveyById
+    loadSurveyResultStub: LoadSurveyResult
+  }
+  const makeSut = (): SutTypes => {
+    const loadSurveyByIdStub = mockLoadSurveyById()
+    const loadSurveyResultStub = mockLoadSurveyResult()
+    const sut = new LoadSurveyResultController(loadSurveyByIdStub, loadSurveyResultStub)
+    return {
+      sut,
+      loadSurveyByIdStub,
+      loadSurveyResultStub
+    }
+  }
+
   test('should call LoadSurveyById with correct values', async () => {
     const { sut, loadSurveyByIdStub } = makeSut()
     const loadByIdSpy = jest.spyOn(loadSurveyByIdStub, 'loadById')
@@ -76,5 +80,13 @@ describe('LoadSurveyResult Controller', () => {
       params: { surveyId: 'any_id' }
     })
     expect(response).toEqual(serverError(new ServerError('')))
+  })
+
+  test('should return 200 on success', async () => {
+    const { sut } = makeSut()
+    const response = await sut.handle({
+      params: { surveyId: 'any_id' }
+    })
+    expect(response).toEqual(ok(mockSurveyResultModel()))
   })
 })
